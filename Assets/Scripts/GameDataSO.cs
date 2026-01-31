9d9dcd038e26c9876da30fc875f2f25943b1c60e
Pyry 'Puri' Kosola <p.kosola@gmail.com>
2026-01-31 16:51:11 +0200

    Add trick calculations to GameDataSO

--- Assets/Scripts/GameDataSO.cs
+++ Assets/Scripts/GameDataSO.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameDataSO", menuName = "ScriptableObjects/GameDataSO")]
 public class GameDataSO : ScriptableObject
 {
-    public int score;
+    // Laps & Time
     public int currentLap = 1;
     public int maxLap;
     public float bestLapTime;
@@ -14,8 +14,39 @@ public class GameDataSO : ScriptableObject
     public float currentTotalTime;
     public float currentLapTime;
     
+    // Trick
+    public int score;
+    public const float TrickBaseScore = 100;
+    private string _lastTrickName = "none";
+    private int _trickMultiplier = 1;
+    private float _trickPointPool = 0;
+    private int _consecutiveTricks = 1;
+    
+    public void DoTrick(TrickData trick)
+    {
+        // If new trick - increase multi
+        if(trick.name != _lastTrickName)
+        {
+            _trickMultiplier++;
+            _trickPointPool += TrickBaseScore;
+            _lastTrickName = trick.name;
+            _consecutiveTricks = 1;
+        }else
+        {
+            _consecutiveTricks++;
+            // Reduce base score if consecutive tricks
+            _trickPointPool = TrickBaseScore / _consecutiveTricks;
+            _lastTrickName = trick.name;
+        }
+    }
+    // Calculate score after enough time elapsed after last trick
+    public void CalculateTrickScore()
+    {
+       score += Mathf.FloorToInt(_trickPointPool * _trickMultiplier);
+    }
+    
+    // String properties to get formatted string for UI
     [CreateProperty] private string LapString => $"{currentLap}/{maxLap}";
-
     [CreateProperty]
     private string LapTime =>
         $"{Mathf.FloorToInt(currentLapTime % 60):00}:{Mathf.RoundToInt((currentLapTime % 1f) * 1000f):000}";
@@ -31,5 +62,7 @@ public class GameDataSO : ScriptableObject
         bestLapTime = 0;
         bestTotalTime = 0;
         lastLapTime = 0;
+        _trickMultiplier = 1;
+        _trickPointPool = 0;
     }
 }
