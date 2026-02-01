ee17ded9c2c1d31ac6ee00150956f5a429e53fa3
Lassi Niinikorpi <lassi.n94@gmail.com>
2026-02-01 09:15:55 +0200

    Fix falling into water when in ragdoll softlock

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1107,6 +1107,11 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Water")) {
+            if (isRagdoll)
+            {
+                return;
+            }
+
             EnterRagdoll(true);
         }
     }
